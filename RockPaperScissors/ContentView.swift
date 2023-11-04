7ed117ae2bcb52afb7f0f856f435d2e1367f373f
Alex Zhulai <Lepiozdyx@gmail.com>
2023-11-04 13:21:29 -0300

    Do Challenge

--- RockPaperScissors/ContentView.swift
+++ RockPaperScissors/ContentView.swift
@@ -8,14 +8,90 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var moves = ["🤜🏼", "🤚🏼", "✌🏼"]
+    @State private var currentChoice = Int.random(in: 0...2)
+    @State private var shouldWin = Bool.random()
+    @State private var score = 0
+    @State private var questionCount = 1
+    @State private var isShowingAlert = false
+    
+    let maxQuestions = 10
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack(spacing: 20) {
+            Spacer()
+            
+            Text("\(questionCount <= maxQuestions ? "Round \(questionCount)" : " Game Over")")
+                .font(.title)
+                        
+            VStack(spacing: 20) {
+                Text("Move: \(moves[currentChoice])")
+                    .font(.system(size: 60))
+                Text("You must \(shouldWin ? "Win" : "Lose")")
+                    .font(.title)
+            }
+            .padding(20)
+            .background(.regularMaterial)
+            .clipShape(.rect(cornerRadius: 20))
+            
+            Spacer()
+            
+            Section("Make your choice:") {
+                HStack(spacing: 20) {
+                    ForEach(0..<3) { number in
+                            Button(moves[number], action: { buttonTapped(number) })
+                    }
+                    .buttonStyle(.bordered)
+                    .font(.system(size: 80))
+                }
+            }
+            
+            Spacer()
+            
+            Text("Your score: \(score)")
+                .font(.title)
+            
+            Spacer()
+            Spacer()
         }
-        .padding()
+        .alert("Game Over", isPresented: $isShowingAlert) {
+            Button("Restart", action: { reset() })
+        } message: {
+            Text("Your final score is \(score)")
+        }
+    }
+    
+    private func buttonTapped(_ answer: Int) {
+        let winningMove = (currentChoice + 1) % 3
+        let losingMove = (currentChoice + 2) % 3
+        let correctAnswer = shouldWin ? winningMove : losingMove
+        
+        if answer != currentChoice {
+            if answer == correctAnswer {
+                score += 1
+            } else {
+                score -= 1
+            }
+        }
+        
+        questionCount += 1
+        
+        if questionCount > maxQuestions {
+            isShowingAlert = true
+        } else {
+            nextRound()
+        }
+    }
+    
+    private func nextRound() {
+        currentChoice = Int.random(in: 0...2)
+        shouldWin.toggle()
+    }
+    
+    private func reset() {
+        score = 0
+        questionCount = 1
+        nextRound()
     }
 }
 
